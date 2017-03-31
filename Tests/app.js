439d1e43c23f722ead7a8b21f8c5f44fb321400d
pumanitro <pumanitro@gmail.com>
2017-03-31 22:03:02 +0200

    Initial commit

---
+++ Tests/app.js
@@ -0,0 +1,8 @@
+// Your Javascript Code Goes Here
+
+var a = 3;
+var b = 4;
+
+console.log(a+b);
+
+// Note: The 'starter' folders that appear in this course's downloadable source code is here to give you a starting point to try out coding yourself. The 'finished' folder contains the code as it is when we finish the lecture. You can use the 'finished' folder to compare to your own code, or just examine the finished code.
