919ffec1a88eb10ba3c9668c994e42642e26596d
pumanitro <pumanitro@gmail.com>
2017-04-07 17:53:47 +0200

    Added second module in another file and extract just one function from export.modules object.

--- Tests/app.js
+++ Tests/app.js
@@ -1,3 +1,7 @@
 
-var greet = require('./hello.js');
+var greet = require('./hello');
 greet();
+
+var greet2 = require('./hello2').greet;
+
+greet2();
---
+++ Tests/hello2.js
@@ -0,0 +1,7 @@
+/**
+ * Created by puman on 07.04.2017.
+ */
+
+module.exports.greet = function(){
+  console.log('Hello world!');
+};
