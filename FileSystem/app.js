4c108f8bb6de40b442d1d6b3715c40ec89ec1999
pumanitro <pumanitro@gmail.com>
2017-04-10 10:02:03 +0200

    Readed file synchronously and synch .

---
+++ FileSystem/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by puman on 10.04.2017.
+ */
+
+const fs = require('fs');
+
+let greet = fs.readFileSync('greet.txt','utf-8');
+console.log(greet);
+
+let greet2 = fs.readFile('greet.txt','utf-8',(err,data)=>{
+    if (err) throw err;
+    console.log(data);
+});
