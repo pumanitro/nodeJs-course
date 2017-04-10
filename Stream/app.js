dd21a796d4d0b00bd3c45a2cb08d7f7183f4a59b
pumanitro <pumanitro@gmail.com>
2017-04-10 23:34:32 +0200

    Compressed data from one file to another by using pipes and method chaining.

--- Stream/app.js
+++ Stream/app.js
@@ -3,17 +3,25 @@
  */
 
 const fs = require('fs');
+const zlib = require('zlib');
 
 let readable = fs.createReadStream('text.txt',{
-    highWaterMark : 10 * 1024,
     encoding : 'utf-8'
 });
 
 let writable = fs.createWriteStream('greetCopy.txt');
 
+//readable.pipe(writable);
+
+/*
 readable.on('data',function(chunk){
     console.log(chunk);
     writable.write(chunk);
 });
+*/
 
+let compressed = fs.createWriteStream('greetCopy.txt.gz');
 
+let gzip = zlib.createGzip();
+
+readable.pipe(gzip).pipe(compressed);
