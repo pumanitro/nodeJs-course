ad3b107c48553b09ed5222e7457f29fb8a49ca1d
pumanitro <pumanitro@gmail.com>
2017-04-13 17:09:23 +0200

    Readed html from external file and transfer it into node server.

--- Web Serwer/app.js
+++ Web Serwer/app.js
@@ -3,11 +3,13 @@
  */
 
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req,res) {
 
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('Hello world\n');
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    var html = fs.readFileSync('index.html');
+    res.end(html);
 
-}).listen(1337,'127.0.0.1');
+}).listen(1337,'localhost');
 
